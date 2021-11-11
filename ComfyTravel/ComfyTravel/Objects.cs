fc30de593f3bd2d2f10f3ad1186a6e6a4acd2550
zhabalyak <zhabalyak@mail.ru>
2021-11-11 10:44:44 +0300

    Создала проект
    .net 5.0, подключила дополнительно mongodb.driver, создала один класс маппинг для объектов, добавила строку подключения к БД и в стартапе в конфигурации добавила монгоклиента

---
+++ ComfyTravel/ComfyTravel/Objects.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComfyTravel
+{
+    [BsonIgnoreExtraElements]
+    public class Objects
+    {
+        [BsonId]
+        public int Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("type")]
+        public string Type { get; set; }
+
+        [BsonElement("coord")]
+        public Tuple<double, double> Coordinates { get; set; }
+    }
+}
