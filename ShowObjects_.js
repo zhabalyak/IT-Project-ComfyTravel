16a221361704bd6a98535efb5b762d231e256473
KMn04 <92161844+KMn04@users.noreply.github.com>
2021-11-15 20:53:36 +0300

    Add files via upload

---
+++ ShowObjects_.js
@@ -0,0 +1,31 @@
+ymaps.ready(function ()
+{
+    var myMap = new ymaps.Map('map', {
+            center: [55.806059, 49.177079],
+            zoom: 11,
+            controls: ['zoomControl']
+        }),
+
+        button_show = document.getElementById("btn_show_map");
+
+
+    button_show.addEventListener("click", function(e) {
+        points_pack = get_points()
+        points = points_pack[0]
+        points_names = points_pack[1]
+
+        // for (point of points)
+        for (i = 0; i < points.length; i++)
+        {
+            myPlacemark = new ymaps.Placemark(points[i], {hintContent: points_names[i]});
+            myMap.geoObjects.add(myPlacemark);
+        }
+    });
+
+    function get_points (e) {
+        var points = [[[55.806059, 49.177076], [55.811681, 49.100693]],
+            ["точка 1", "точка 2"]]
+        return points;
+    }
+
+});
