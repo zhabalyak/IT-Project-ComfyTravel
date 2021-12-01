8cc4d8d0093e8256de15fe8808c507aad65bf488
KMn04 <92161844+KMn04@users.noreply.github.com>
2021-12-01 20:14:26 +0300

    Delete map.js

--- map.js
+++
@@ -1,7 +0,0 @@
-ymaps.ready(function(){
-    let myMap = new ymaps.Map('map', {
-            center: [55.806059, 49.177079],
-            zoom: 11,
-            controls: ['zoomControl']
-        })
-});
