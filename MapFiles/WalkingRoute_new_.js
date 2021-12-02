0757cb416cef247aaacef867738d1cd889ddd1b7
IgelSchnauze <vikuscha10@mail.ru>
2021-12-02 15:48:53 +0300

    v.2 рисовка маршрута на карте

--- MapFiles/WalkingRoute_new_.js
+++ MapFiles/WalkingRoute_new_.js
@@ -9,9 +9,11 @@ ymaps.ready(function () {
 
     var points_pack_x = document.getElementById('points_x').textContent.split(", ");
     var points_pack_y = document.getElementById('points_y').textContent.split(", ");
+    var points_names = document.getElementById('points_names').textContent.split(", ");
     var route_mode = document.getElementById('route_mode').textContent.split(", ");
     //mode = 1, тогда пешеходный/общ транспорт, 
     //     = 2, тогда на машине
+    //     = 3, тогда обществ транспорт
 
     //console.log(points_pack_x);
     //console.log(points_pack_y);
@@ -20,14 +22,15 @@ ymaps.ready(function () {
 
     //console.log(points_pack);
 
-    show_route_on_map(points_pack, route_mode);
+    show_route_on_map(points_pack, points_names, route_mode);
 
-    function show_route_on_map (points_pack, mode) 
+    function show_route_on_map (points_pack, points_names, mode) 
     {
         //points parsing
         points = new Array(points_pack[0].length);
         for (i = 0; i < points_pack[0].length; i++) {
-            points[i] = [parseFloat(points_pack[0][i].replace(",", ".")), parseFloat(points_pack[1][i].replace(",", "."))];
+            points[i] = [parseFloat(points_pack[0][i].replace(",", ".")), 
+                         parseFloat(points_pack[1][i].replace(",", "."))];
         }
 
 
@@ -35,17 +38,21 @@ ymaps.ready(function () {
         {
             referencePoints: points,
             params: {
-                //nип маршрутизации - пешеходный ('auto' - для машины)
+                //nип маршрутизации - пешеходный
                 routingMode: 'pedestrian' 
             }
         }, {
-        // чтобы сделать точки А, Б... невидимыми
-            //wayPointVisible:false,
-        // внешний вид линии пешеходного маршрута
-            routeActivePedestrianSegmentStrokeStyle: "solid",
-            routeActivePedestrianSegmentStrokeColor: "#56f067",
+        // сделать точки А, Б... невидимыми
+            wayPointVisible:false,
+        // убрать метки с км для пешеходного типа
+            routeActiveMarkerVisible: false,
+        // откл возможность кликать на маршрут
+            routeOpenBalloonOnClick: false,
         // для авто-маршрута
+            routeActiveStrokeStyle: "solid",
             routeActiveStrokeColor: "#E63E92",
+        // внешний вид линии пешеходного маршрута
+            routeActivePedestrianSegmentStrokeColor: "#56f067",
         // фокусировка карты на маршруте (его целиком видно дб)
             boundsAutoApply: true
         });
@@ -54,8 +61,20 @@ ymaps.ready(function () {
         {
             multiRoute.model.setParams({routingMode: 'auto'})
         }
-
+        // if (mode == 3)
+        // {
+        //     multiRoute.model.setParams({routingMode: 'masstransit'})
+        // }
         myMap.geoObjects.add(multiRoute);
+
+
+        for (i = 0; i < points.length; i++)
+        {
+            myPlacemark = new ymaps.Placemark(points[i], 
+                          {hintContent: points_names[i], iconContent: i+1});
+            myMap.geoObjects.add(myPlacemark);
+        }  
+
     } 
 
 });
